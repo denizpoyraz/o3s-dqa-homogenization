4e07df0820454cb95b81b38aef5b359953ecff92
denizpoyraz <deniz.poyraz@meteo.be>
2021-07-09 13:57:00 +0200

    update-test

--- aura-mls/heatmap_stepbystep.py
+++ aura-mls/heatmap_stepbystep.py
@@ -5,80 +5,38 @@ import seaborn as sns
 import datetime
 
 
-# dfm = pd.read_csv('/home/poyraden/Analysis/Homogenization_public/Files/madrid/DQA_upd/Madrid_Metada_DQA.csv')
-# dfm['DateTime'] = dfm['DateTime'].apply(lambda x: pd.to_datetime(str(x), format='%Y%m%d %H:%M:%S'))
-# # dfm['Date'] = dfm['DateTime'].apply(lambda _: datetime.strftime(_, '%Y-%m-%d'))
-# dfm['Date'] = dfm['DateTime'].apply(lambda x: x.strftime('%Y-%m-%d'))
-
-# dfm['Date'] = dfm['DateTime'].apply(lambda x: x.date())
-
-# path = '/home/poyraden/Analysis/Homogenization_public/Files/uccle/Binned/'
+path = '/home/poyraden/Analysis/Homogenization_public/Files/madrid/DQA_nors80/Binned/'
+df1 = pd.read_csv(path + 'MadridInterpolated_dqa_nors80_v2.csv')
 # df1 = pd.read_csv(path + 'UccleInterpolated_dqa_nors80.csv')
-# df2 = pd.read_csv(path + 'UccleInterpolated_dqa_rs80.csv')
 
-path = '/home/poyraden/Analysis/Homogenization_public/Files/madrid/DQA_final/Binned/'
+# df1['DateTime'] = df1['Date'].apply(lambda x: pd.to_datetime(str(x), format='%Y-%m-%d'))
+df1['DateTime'] = pd.to_datetime(df1['Date'], format='%Y-%m-%d')
 
-df1 = pd.read_csv(path + 'MadridInterpolated_dqa_rs80_finalfinal.csv')
-df2 = pd.read_csv(path + 'MadridInterpolated_dqa_nors80.csv')
-
-
-print('one', len(df1), len(df2))
-
-# fill missing dates
-df1['DateTime'] = df1['Date'].apply(lambda x: pd.to_datetime(str(x), format='%Y-%m-%d'))
 df1['date'] = df1['DateTime'].apply(lambda x: x.date())
 df1.date = pd.to_datetime(df1.date)
 
-df2['DateTime'] = df2['Date'].apply(lambda x: pd.to_datetime(str(x), format='%Y-%m-%d'))
-df2['date'] = df2['DateTime'].apply(lambda x: x.date())
-df2.date = pd.to_datetime(df2.date)
-
-# df1 = df1.set_index('date')
+df1 = df1.set_index('date')
 # df2 = df2.set_index('date')
+
+# l1 = df1.drop_duplicates(['Date']).Date.tolist()
+# l2 = df2.drop_duplicates(['Date']).Date.tolist()
 #
-# d1 = df1.resample('1W').first().fillna(0)
-# d2 = df2.resample('1W').first().fillna(0)
-# d2 = df2.resample('1W').first().fillna(0)
-#
-# df1 = pd.concat([df1, d1[~d1.index.isin(df1.index)]]).sort_index()
-# df2 = pd.concat([df2, d2[~d2.index.isin(df2.index)]]).sort_index()
-#
-# df1 = df1.reset_index()
-# df2 = df2.reset_index()
-
-print('two', len(df1), len(df2))
-
-
-
-# path = '/home/poyraden/Analysis/Homogenization_public/Files/sodankyla/Binned/'
-# df1 = pd.read_csv(path + 'SodankylaInterpolated_dqa_nors80.csv')
-# df2 = pd.read_csv(path + 'SodankylaInterpolated_dqa_rs80.csv')
-
-
-# # #
-l1 = df1.drop_duplicates(['Date']).Date.tolist()
-l2 = df2.drop_duplicates(['Date']).Date.tolist()
-
-common_dates12 = list(set(l1).intersection(set(l2)))
-print(len(common_dates12), common_dates12[0:3])
-
-# l3 = dfm.drop_duplicates(['DateTime']).Date.tolist()
-# print('l3', len(l3), l3)
-# common_datesall = list(set(l3).intersection(set(common_dates12)))
+# common_dates12 = list(set(l1).intersection(set(l2)))
+# print(len(common_dates12), common_dates12[0:3])
 
 # df1 = df1[df1['Date'].isin(common_dates12)]
 # df2 = df2[df2['Date'].isin(common_dates12)]
 
-df1 = df1[df1['Date'].isin(common_dates12)]
-df2 = df2[df2['Date'].isin(common_dates12)]
-# dfm = dfm[dfm['Date'].isin(common_datesall)]
-
-
-# print(len(df1.drop_duplicates(['Date'])), len(df2.drop_duplicates(['Date'])), len(dfm.drop_duplicates(['Date'])))
-print(len(df1.drop_duplicates(['Date'])), len(df2.drop_duplicates(['Date'])))
+# df1 = df1[df1['Date'].isin(common_dates12)]
+# df2 = df2[df2['Date'].isin(common_dates12)]
+# print(len(df1.drop_duplicates(['Date'])), len(df2.drop_duplicates(['Date'])))
 
 df1 = df1[df1.PreLevel > 7]
-df2 = df2[df2.PreLevel > 7]
+
+df1['PreLevel'] = df1['PreLevel'].astype(int)
+df1['DateTime'] = pd.to_datetime(df1['Date'], format='%Y-%m-%d')
+
+
 
 # Plotname = 'RS80_vs_noRS80'
 # heatmap_label = 'RS80 - NoRS80 / NoRS80  (%)'
@@ -86,23 +44,14 @@ df2 = df2[df2.PreLevel > 7]
 # # #
 # df1['RDif_UcIntLin'] = 100 * (np.asarray(df2.PO3_UcIntLin_dqa) - np.asarray(df1.PO3_UcIntLin_dqa)) / np.asarray(df1.PO3_UcIntLin_dqa)
 
-# df1['DateTime'] = df1['Date'].apply(lambda x: pd.to_datetime(str(x), format='%Y%m%d'))
-df1['DateTime'] = pd.to_datetime(df1['Date'], format='%Y-%m-%d')
-
-# df1['date'] = df1['DateTime'].Date
-df1['date'] = df1['DateTime'].apply(lambda x: x.date())
-df1['DateTime2'] = df1["DateTime"].dt.strftime('%Y-%m-%d')
-
-
-
 # (2 - 1) / 1
 # Plotname = 'Eta_vs_Raw_alltimerange'
 # heatmap_label = 'Eta - NoCorrection / NoCorrection  (%)'
 # ptitle = 'Effect of Conversion Efficiency Correction'
 #
-Plotname = 'EtaBkg_vs_Eta_alltimerange_unzoom'
-heatmap_label = 'EtaBkg - Eta / Eta  (%)'
-ptitle = 'Effect of Background Current Correction'
+# Plotname = 'EtaBkg_vs_Eta_alltimerange'
+# heatmap_label = 'EtaBkg - Eta / Eta  (%)'
+# ptitle = 'Effect of Background Current Correction'
 
 # Plotname = 'EtaBkgTpump_vs_EtaBkg_alltimerange'
 # heatmap_label = 'EtaBkgTpump - EtaBkg / EtaBkg  (%)'
@@ -110,13 +59,13 @@ ptitle = 'Effect of Background Current Correction'
 
 # Plotname = 'EtaBkgTpumpPhigr_vs_EtaBkgTpump_alltimerange'
 # heatmap_label = 'EtaBkgTpumpPhigr - EtaBkgTpump / EtaBkgTpump  (%)'
-# ptitle = 'Effect of Pump Flow Rate (humidity) Correction'
+# ptitle = 'Effect of Pump Flow Rate (temperature) Correction'
 #
 # Plotname = 'EtaBkgTpumpPhigrPhiEff_vs_EtaBkgTpumpPhigr_alltimerange'
 # heatmap_label = 'EtaBkgTpumpPhigrPhiEff - EtaBkgTpumpPhigr / EtaBkgTpumpPhigr  (%)'
 # ptitle = 'Effect of Pump Flow Efficiency Correction'
-
-# Plotname = 'DQA_vs_Raw_alltimerange_v2'
+# #
+# Plotname = 'DQA_vs_Raw_alltimerange_unzoom'
 # heatmap_label = 'DQA - NoCorrection / NoCorrection  (%)'
 # ptitle = 'Effect all DQA Corrections'
 
@@ -124,162 +73,62 @@ ptitle = 'Effect of Background Current Correction'
 # heatmap_label = 'DQA - NDACC / NDACC  (%)'
 # ptitle = 'DAQ-O3S vs NDACC-O3S'
 
-Plotname = 'DQA_vs_WOUDC_alltimerange_v2'
-heatmap_label = 'DQA - WOUDC / WOUDC  (%)'
-ptitle = 'DAQ-O3S vs WOUDC-O3S'
+# Plotname = 'DQA_vs_WOUDC_alltimerange'
+# heatmap_label = 'DQA - WOUDC / WOUDC  (%)'
+# ptitle = 'DAQ-O3S vs WOUDC'
 
-min = 0
-max = 5
+Plotname = 'WOUDC_vs_WOUDCorig_alltimerange'
+heatmap_label = 'DQA - WOUDC / WOUDC  (%)'
+ptitle = 'DAQ-O3S vs WOUDC'
+
+min = -3
+max = 3
 # df1['RDif_UcIntLin'] = 100 * (np.asarray(df1.PO3_UcIntLin_eta) - np.asarray(df1.PO3_UcIntLin_nc)) / np.asarray(df1.PO3_UcIntLin_nc)
 # df1['RDif_UcIntLin'] = 100 * (np.asarray(df1.PO3_UcIntLin_etabkg) - np.asarray(df1.PO3_UcIntLin_eta)) / np.asarray(df1.PO3_UcIntLin_eta)
 # df1['RDif_UcIntLin'] = 100 * (np.asarray(df1.PO3_UcIntLin_etabkgtpump) - np.asarray(df1.PO3_UcIntLin_etabkg)) / np.asarray(df1.PO3_UcIntLin_etabkg)
 # df1['RDif_UcIntLin'] = 100 * (np.asarray(df1.PO3_UcIntLin_etabkgtpumpphigr) - np.asarray(df1.PO3_UcIntLin_etabkgtpump)) / np.asarray(df1.PO3_UcIntLin_etabkgtpump)
 # df1['RDif_UcIntLin'] = 100 * (np.asarray(df1.PO3_UcIntLin_dqa) - np.asarray(df1.PO3_UcIntLin_etabkgtpumpphigr)) / np.asarray(df1.PO3_UcIntLin_etabkgtpumpphigr)
 # df1['RDif_UcIntLin'] = 100 * (np.asarray(df1.PO3_UcIntLin_dqa) - np.asarray(df1.PO3_UcIntLin_nc)) / np.asarray(df1.PO3_UcIntLin_nc)
-df1['RDif_UcIntLin'] = 100 * (np.asarray(df1.PO3_UcIntLin_dqa) - np.asarray(df1.PO3_UcIntLin)) / np.asarray(df1.PO3_UcIntLin)
-
-print('size bin', len(df1.drop_duplicates(['Date']).Date.tolist()) / 24)
-
-df1['PreLevel'] = df1['PreLevel'].astype(int)
-print('Date min, max', df1.Date.min(), df1.Date.max())
-
-# df1 = df1[(df1['Date'] > 19981101) & (df1['Date'] < 19981212)]
-
-# t = df1.pivot_table(index='PreLevel', columns='Date', values='RDif_UcIntLin')
-t = df1.pivot_table(index='PreLevel', columns='date', values='RDif_UcIntLin', fill_value = 0, dropna = True)
-
-# print('dropna true', len(t.index), len(t.columns))
-#
-# t = df1.pivot_table(index='PreLevel', columns='date', values='RDif_UcIntLin', fill_value = 0, dropna = False)
-#
-# print('dropna false', len(t.index), len(t.columns))
-# print('size bin',  len(df1.drop_duplicates(['Date']).Date.tolist()) , len(df1), len(df1.drop_duplicates(['Date']).Date.tolist()) / 25)
+# df1['RDif_UcIntLin'] = 100 * (np.asarray(df1.PO3_UcIntLin_dqa) - np.asarray(df1.PO3_UcIntLin)) / np.asarray(df1.PO3_UcIntLin)
+df1['RDif_UcIntLin'] = 100 * (np.asarray(df1.PO3_UcIntLin_woudc) - np.asarray(df1.PO3_UcIntLin)) / np.asarray(df1.PO3_UcIntLin)
+# df1['RDif_UcIntLin'] = 100 * (np.asarray(df1.PO3_UcIntLin_woudc_v2) - np.asarray(df1.PO3_UcIntLin)) / np.asarray(df1.PO3_UcIntLin)
 
 
-# xfreq = 134 #uccle mls range
-# xfreq = 55 #uccle all range
-# xfreq = 53 #uccle all range
-# xfreq = 35 #madrid
-# xfreq = 39 #madrid ymd2
-xfreq = 46 #madrid ymd3
 
-# xfreq = 50 #sodankyla all range
-# xfreq = 38
-# for all range xtick labels
-# xtick_labels = [1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012,
-#                 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
-xtick_labels = ['1994', '1995', '1996', '1997', '1998', '1999','2001', '2005', '2006', '2007', '2008', '2009', '2010',
-                '2011', '2012', '2013', '2014', '2015','2019', '2020', '2021']
+t = df1.pivot_table(index='PreLevel', columns='DateTime', values='RDif_UcIntLin', fill_value = 0, dropna = False)
+min_dist_days = t.columns.to_series().diff()
+min_mean = min_dist_days.median()
+print('min distance 2 launches', min_mean )
+#resample to see missing dates
+t = t.T.resample(min_mean).mean().T
+
+x_min_mean = int(str(min_mean.days))
+labels = t.columns.year.unique()
+xfreq = int(365/x_min_mean)
+print('xfreq', xfreq)
 
 
+
+df1.Date = pd.to_datetime(df1.Date)
 #Plotting
 # ########################################################################################################################
-#original plot without second TO plot
 fig, ax = plt.subplots(figsize=(17, 9))
 ax.set_yscale('log')
-# hm = sns.heatmap(t, vmin=-10, vmax=10 , cmap="vlag", xticklabels=xfreq, yticklabels=2, square=True,
-#                  cbar_kws={'label': heatmap_label})
-# hm = sns.heatmap(t, vmin=min, vmax=max , cmap="vlag", cbar_kws={'label': heatmap_label})
-hm = sns.heatmap(t, vmin=min, vmax=max , cmap="vlag", xticklabels=xfreq,
-                 cbar_kws={'label': heatmap_label})
 
-ax.set_xticklabels(xtick_labels, rotation=0)
+ax = sns.heatmap(t, vmin=min, vmax=max , cmap="vlag", cbar_kws={'label': heatmap_label}, xticklabels=xfreq)
+ax.set_xticklabels(labels, rotation=0)
+
 plt.yticks(fontsize=6)
 # ax.set_yticklabels(ytick_labels, rotation = 0)
-# plt.xticks(rotation = 45)
+plt.xticks(rotation = 90)
 plt.xticks(fontsize=8)
 plt.xlabel(" ")
-# ax.set_ylim([68,8])
+
 plt.title(ptitle)
 
-plt.savefig(path + 'Plots/' + Plotname + '.png')
-plt.savefig(path + 'Plots/' + Plotname + '.eps')
-plt.savefig(path + 'Plots/' + Plotname + '.pdf')
+plt.savefig(path + 'Plots/' + Plotname + '_rs.png')
+plt.savefig(path + 'Plots/' + Plotname + '_rs.eps')
+plt.savefig(path + 'Plots/' + Plotname + '_rs.pdf')
 
 plt.show()
- ########################################################################################################################
-# fig, ax1 = plt.subplots(figsize=(17, 9))
-#
-# plt.plot(dfm.Date, dfm.O3ratio)
-# plt.plot(dfm.Date, dfm.O3ratio_hom)
-# plt.xticks(np.arange(0, len(dfm)+1, xfreq))
-# ax1.set_xticklabels(xtick_labels, rotation=0)
-#
-#
-# plt.show()
 
-
-# # two plots one for TO plt
-#
-#
-# fig = plt.figure(figsize=(17,9))
-# ax1 = plt.subplot(2, 1, 1)
-    # ax1.set_yscale('log')
-# # hm = sns.heatmap(t, vmin=-10, vmax=10 , cmap="vlag", xticklabels=xfreq, yticklabels=2, square=True,
-# #                  cbar_kws={'label': heatmap_label})
-# # hm = sns.heatmap(t, vmin=min, vmax=max , cmap="vlag", cbar_kws={'label': heatmap_label})
-# hm = sns.heatmap(t, vmin=min, vmax=max , cmap="vlag", xticklabels=xfreq,
-#                  cbar_kws={'label': heatmap_label})
-#
-# ax1.set_xticklabels(xtick_labels, rotation=0)
-# plt.yticks(fontsize=6)
-# # ax.set_yticklabels(ytick_labels, rotation = 0)
-# # plt.xticks(rotation = 45)
-# plt.xticks(fontsize=8)
-# plt.xlabel(" ")
-# # ax.set_ylim([68,8])
-# plt.title(ptitle)
-#
-# x0,x1 = ax1.get_xlim()
-# y0,y1 = ax1.get_ylim()
-# # ax1.set_aspect((x1-x0)/(y1-y0))
-# ax1.set_aspect('equal')
-#
-# ax2 = plt.subplot(2, 1, 2)
-# plt.plot(dfm.Date, dfm.O3ratio)
-# plt.plot(dfm.Date, dfm.O3ratio_hom)
-# plt.xticks(np.arange(0, len(dfm)+1, xfreq))
-# ax2.set_xticklabels(xtick_labels, rotation=0)
-# ax2.set_aspect('equal')
-#
-#
-# plt.show()
-
-# gs_top = plt.GridSpec(2, 1, height_ratios=[3,1], hspace=0.05)
-# gs_bottom = plt.GridSpec(2, 1, height_ratios=[3,1], hspace=0.25)
-#
-# fig = plt.figure(figsize=(17, 9))
-# # fig, (ax, ax1    ) = plt.subplots(2,1, figsize=(17,9), gridspec_kw={'height_ratios':[3, 1]})
-# ax = fig.add_subplot(gs_top[0,:])
-# ax.set_yscale('log')
-# # hm = sns.heatmap(t, vmin=-10, vmax=10 , cmap="vlag", xticklabels=xfreq, yticklabels=2, square=True,
-# #                  cbar_kws={'label': heatmap_label})
-# # hm = sns.heatmap(t, vmin=min, vmax=max , cmap="vlag", cbar_kws={'label': heatmap_label})
-# hm = sns.heatmap(t, vmin=min, vmax=max , cmap="vlag", xticklabels=xfreq,
-#                  cbar_kws={'label': heatmap_label})
-#
-# ax.set_xticklabels(xtick_labels, rotation=0)
-# plt.yticks(fontsize=6)
-# # ax.set_yticklabels(ytick_labels, rotation = 0)
-# # plt.xticks(rotation = 45)
-# plt.xticks(fontsize=8)
-# plt.xlabel(" ")
-# # ax.set_ylim([68,8])
-# plt.title(ptitle)
-#
-# ax1 = fig.add_subplot(gs_bottom[1,:])
-# plt.plot(dfm.Date, dfm.O3ratio)
-# plt.plot(dfm.Date, dfm.O3ratio_hom)
-# plt.xticks(np.arange(0, len(dfm)+1, xfreq))
-# ax1.set_xticklabels(xtick_labels, rotation=0)
-# x0,x1 = ax1.get_xlim()
-# y0,y1 = ax1.get_ylim()
-# ax1.set_aspect(1)
-#
-#
-#
-# # plt.savefig(path + 'Plots/' + Plotname + '.png')
-# # plt.savefig(path + 'Plots/' + Plotname + '.eps')
-# # plt.savefig(path + 'Plots/' + Plotname + '.pdf')
-#
-# plt.show()
--- aura-mls/mls_heatmap.py
+++ aura-mls/mls_heatmap.py
@@ -6,53 +6,19 @@ import matplotlib.dates as mdates
 
 
 path = '/home/poyraden/Analysis/Homogenization_public/Files/madrid/MLS/'
-df2 = pd.read_csv(path + 'MLS_MadridInterpolated_woudc_v05_rs80.csv')
-df1 = pd.read_csv(path + 'MLS_MadridInterpolated_dqa_rs80_v05.csv')
+# df2 = pd.read_csv(path + 'MLS_MadridInterpolated_woudc_v05_rs80.csv')
+df1 = pd.read_csv(path + 'MLS_MadridInterpolated_woudc_nors80_v04.csv')
 
-# path = '/home/poyraden/Analysis/Homogenization_public/Files/uccle/MLS/'
-### not all the data series have the same dates, raw + DQA and presto ones
-# df1 = pd.read_csv(path + 'MLS_UccleInterpolated_dqa_rs80_v05.csv')
-# df2 = pd.read_csv(path + 'MLS_UccleInterpolated_dqa_rs80_v05.csv')
 
 df1 = df1[df1.PreLevel < 216]
 df1 = df1[df1.PreLevel >= 8]
-df2 = df2[df2.PreLevel < 216]
-df2 = df2[df2.PreLevel >= 8]
 
-print(len(df1), len(df2))
-
-l1 = df1.drop_duplicates(['Date']).Date.tolist()
-l2 = df2.drop_duplicates(['Date']).Date.tolist()
-
-common_dates12 = list(set(l1).intersection(set(l2)))
-print(len(common_dates12))
-
-df1 = df1[df1['Date'].isin(common_dates12)]
-df2 = df2[df2['Date'].isin(common_dates12)]
-
-print(len(df1), len(df2))
-
-# # #
-#
-# print(len(df1), len(df2))
-
-# df1 = pd.read_csv(path + 'MLS_MadridInterpolated_o3s_v05_rs80.csv')
-
-# df1 = pd.read_csv(path + 'MLS_MadridInterpolated_o3s_v05.csv')
-# Plotname = 'WOUDC_vs_MLS_v05'
-Plotname = 'DQA_vs_MLS_v05'
+Plotname = 'WOUDC_vs_MLS_v04_nors80'
+# Plotname = 'DQA_vs_MLS_v04_nors80'
 
 # plot_title = 'Madrid (tpump) - MLS comparison'
-# plot_title = 'Madrid O3S-WOUDC - MLS (v05) comparison'
-plot_title = 'Madrid O3S-DQA - MLS (v05) comparison'
-
-# plot_title = 'Uccle O3S-NoCorrection - MLS (v05) comparison'
-
-# ytick_labels = [10, 20, 30, 40, 50]
-# ytick_labels = [8, 10, 12, 14, 17, 21, 26, 31, 38, 46, 56]
-ytick_labels = [8, 10, 12, 14, 17, 21, 26, 31, 38, 46, 56, 68, 82, 100, 121, 146, 177, 215]
-xtick_labels = [ '2005', '2006', '2007', '2008', '2009', '2010',
-                '2011', '2012', '2013', '2014', '2015','2019', '2020', '2021']
+plot_title = 'Madrid O3S-WOUDC - MLS (v04) comparison'
+# plot_title = 'Madrid O3S-DQA - MLS (v04) comparison'
 
 # df1 = df1[df1.PreLevel < 57]
 # df1 = df1[df1.PreLevel >= 8]
@@ -60,63 +26,39 @@ xtick_labels = [ '2005', '2006', '2007', '2008', '2009', '2010',
 # df1 = df1[df1.PreLevel < 50]
 
 df1 = df1[df1.PreLevel >= 8]
-# df2 = df2[df2.PreLevel < 57]
-# df2 = df2[df2.PreLevel >= 8]
-#
-# print(len(df1.drop_duplicates(['Date'])), len(df2.drop_duplicates(['Date'])) )
+
 
 df1['RDif_UcIntLin'] = 100 * (np.asarray(df1.PO3_UcIntLin) - np.asarray(df1.PO3_MLS)) / np.asarray(df1.PO3_UcIntLin)
-df2['RDif_UcIntLin'] = 100 * (np.asarray(df2.PO3_UcIntLin) - np.asarray(df2.PO3_MLS)) / np.asarray(df2.PO3_UcIntLin)
+# df2['RDif_UcIntLin'] = 100 * (np.asarray(df2.PO3_UcIntLin) - np.asarray(df2.PO3_MLS)) / np.asarray(df2.PO3_UcIntLin)
 
-print('size bin', len(df1.drop_duplicates(['Date']).Date.tolist()) / 15.5)
+# df1['DateTime'] = df1['Date'].apply(lambda x: pd.to_datetime(str(x), format='%Y%m%d'))
+df1['DateTime'] = pd.to_datetime(df1['Date'], format='%Y%m%d')
 
-df1['DateTime'] = df1['Date'].apply(lambda x: pd.to_datetime(str(x), format='%Y%m%d'))
 df1['Date'] = df1['DateTime'].apply(lambda x: x.date())
-
-df2['DateTime'] = df2['Date'].apply(lambda x: pd.to_datetime(str(x), format='%Y%m%d'))
-df2['Date'] = df2['DateTime'].apply(lambda x: x.date())
-
-# original
-# xfreq = 38 #sodankyla
-xfreq = 33 #madrid
-
-# xfreq = 140 #uccle
-
-
 df1['PreLevel'] = df1['PreLevel'].astype(int)
 
 
 fig, ax = plt.subplots(figsize=(17, 9))
-
 ax.set_yscale('log')
+t = df1.pivot_table(index='PreLevel', columns='DateTime', values='RDif_UcIntLin', fill_value = 0, dropna = False)
 
-# t = df1.pivot_table(index='PreLevel', columns='Date', values='RDif_UcIntLin', fill_value = 0, dropna = False)
-# t2 = df2.pivot_table(index='PreLevel', columns='Date', values='RDif_UcIntLin', fill_value = 0, dropna = False)
+min_dist_days = t.columns.to_series().diff()
+min_mean = min_dist_days.median()
+print('min distance 2 launches', min_mean )
+#resample to see missing dates
+t = t.T.resample(min_mean).mean().T
 
-t = df1.pivot_table(index='PreLevel', columns='Date', values='RDif_UcIntLin', fill_value = 0)
-t2 = df2.pivot_table(index='PreLevel', columns='Date', values='RDif_UcIntLin', fill_value = 0)
+x_min_mean = int(str(min_mean.days))
+labels = t.columns.year.unique()
+xfreq = int(365/x_min_mean)
+print('xfreq', xfreq)
 
-
-print(t.columns)
-print(t2.columns)
-
-common_dates12 = list(set(t.columns).intersection(set(t2.columns)))
-print(common_dates12)
-
-df1 = df1[df1['Date'].isin(common_dates12)]
-df2 = df2[df2['Date'].isin(common_dates12)]
-
-t = df1.pivot_table(index='PreLevel', columns='Date', values='RDif_UcIntLin', fill_value = 0)
-t2 = df2.pivot_table(index='PreLevel', columns='Date', values='RDif_UcIntLin', fill_value = 0)
-
-print(len(t.index), len(t.columns))
-print(len(t2.index), len(t2.columns))
 
 # sns.color_palette("vlag", as_cmap=True)
 hm = sns.heatmap(t, vmin=-10, vmax=10, cmap="vlag", xticklabels=xfreq,  square=True,
                  cbar_kws={'label': 'ECC - MLS / ECC (%)'})
 
-ax.set_xticklabels(xtick_labels, rotation=0)
+ax.set_xticklabels(labels, rotation=0)
 plt.yticks(fontsize=10)
 # ax.set_yticklabels(ytick_labels, rotation = 0)
 # plt.xticks(rotation = 45)
